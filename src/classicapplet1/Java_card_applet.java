95bdecc64b6b04e1d89d2ed4b62d4b2d788ca5e3
mohammed <mohammed.almissbah@nctr.sd>
2019-10-03 14:46:23 +0200

    add store data feature

--- src/classicapplet1/Java_card_applet.java
+++ src/classicapplet1/Java_card_applet.java
@@ -10,6 +10,7 @@ import javacard.security.KeyBuilder;
 import javacard.security.KeyPair;
 import javacard.security.RSAPrivateKey;
 import javacard.security.RSAPublicKey;
+import javacard.security.Signature;
 
 /**
  *
@@ -32,7 +33,7 @@ public class Java_card_applet extends Applet {
     // maximum size PIN
     final static byte MAX_PIN_SIZE = (byte) 0x08;
     final static short SW_VERIFICATION_FAILED = 0x6312;
-
+    final static short SW_PIN_VERIFICATION_REQUIRED = 0x6311;
     OwnerPIN mPin;
     OwnerPIN mPuk;
     byte[] mData1;
@@ -60,9 +61,10 @@ public class Java_card_applet extends Applet {
     protected Java_card_applet() {
         mKeyPair.genKeyPair();
         mPin = new OwnerPIN(PIN_TRY_LIMIT, MAX_PIN_SIZE);
-
+        mPuk = new OwnerPIN(PIN_TRY_LIMIT, MAX_PIN_SIZE);
         byte[] pinArr = {0, 0, 0, 0};
         mPin.update(pinArr, (short) 0, (byte) pinArr.length);
+        mPuk.update(pinArr, (short) 0, (byte) pinArr.length);
         register();
     }
 
@@ -74,5 +76,81 @@ public class Java_card_applet extends Applet {
      */
     public void process(APDU apdu) {
         //Insert your code here
+        byte[] buffer = apdu.getBuffer();
+        if ((buffer[ISO7816.OFFSET_CLA] == 0)
+                && (buffer[ISO7816.OFFSET_INS] == (byte) (0xA4))) {
+            return;
+        }
+
+        if (buffer[ISO7816.OFFSET_CLA] != APPLET_CLA) {
+            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);
+        }
+
+        switch (buffer[ISO7816.OFFSET_INS]) {
+            case SET_DATA_1:
+                storeData(apdu, (short) 1);
+                return;
+            case SET_DATA_2:
+                storeData(apdu, (short) 2);
+                return;
+            case SET_DATA_3:
+                storeData(apdu, (short) 3);
+                return;
+            case UNBLOCK:
+                mPin.resetAndUnblock();
+                return;
+            case VERIFY:
+                verify(apdu);
+                return;
+
+            default:
+                ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+        }
+
+    }
+
+    private void verify(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        // retrieve the PIN data for validation.
+        byte byteRead = (byte) (apdu.setIncomingAndReceive());
+
+        // check pin
+        // the PIN data is read into the APDU buffer
+        // at the offset ISO7816.OFFSET_CDATA
+        // the PIN data length = byteRead
+        if (mPin.check(buffer, ISO7816.OFFSET_CDATA, byteRead) == false) {
+            ISOException.throwIt(SW_VERIFICATION_FAILED);
+        }
+    }
+
+    private void storeData(APDU apdu, short type) {
+
+        // access authentication
+        if (!mPin.isValidated()) {
+            ISOException.throwIt(SW_PIN_VERIFICATION_REQUIRED);
+        }
+
+        byte[] buffer = apdu.getBuffer();
+        short LC = apdu.getIncomingLength();
+        byte[] store_data = new byte[(short) LC];
+        short recivedDataLength = apdu.setIncomingAndReceive();
+        short dataOffset = apdu.getOffsetCdata();
+
+        Util.arrayCopy(buffer, dataOffset, store_data, (short) 0, LC);
+        apdu.receiveBytes(dataOffset);
+        Util.arrayCopy(buffer, dataOffset, store_data, (short) 0, LC);
+
+        switch (type) {
+            case (short) 1:
+                mData1 = store_data;
+                break;
+            case (short) 2:
+                mData2 = store_data;
+                break;
+            default:
+                mData3 = store_data;
+                break;
+        }
+
     }
 }
